812aec90aeba367a9b30c591fe8e89b368cd75da
PaulUno777 <pauluno777@gmail.com>
2023-02-14 22:12:50 +0100

    update complete search

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -166,225 +166,201 @@ export class SearchService {
   //========= Search  Complete Features ================
 
   async searchComplete(body: SearchCompleteDto) {
-    const sanctionedPipeline = [];
-    const akaPipeline = [];
     let filtered: any[];
-
     if (body.fullName) {
-      //push the first stage in the pipeline
-      //------- sanctioned ----------
-      sanctionedPipeline.push(
-        {
-          $search: {
-            index: 'sanctionned_index',
-            text: {
-              query: body.fullName,
-              path: ['firstName', 'lastName', 'middleName', 'otherNames'],
-              fuzzy: {
-                maxEdits: 2,
-              },
-            },
-          },
-        },
-        {
-          $lookup: {
-            from: 'SanctionList',
-            localField: 'list_id',
-            foreignField: '_id',
-            pipeline: [{ $project: { _id: 0, name: 1 } }],
-            as: 'sanction',
-          },
-        },
-      );
-      //------- akas ---------
-      akaPipeline.push(
-        {
-          $search: {
-            index: 'sanctioned_aka_index',
-            text: {
-              query: body.fullName,
-              path: ['firstName', 'lastName', 'middleName'],
-              fuzzy: {
-                maxEdits: 2,
-              },
-            },
-          },
-        },
-        {
-          $lookup: {
-            from: 'Sanctioned',
-            let: {
-              id: '$sanctionnedId',
-            },
-            pipeline: [
-              {
-                $match: {
-                  $expr: {
-                    $eq: ['$_id', '$$id'],
-                  },
-                },
-              },
-              {
-                $project: {
-                  list_id: 1,
-                  firstName: 1,
-                  middleName: 1,
-                  lastName: 1,
-                  original_name: 1,
-                  otherNames: 1,
-                },
-              },
-            ],
-            as: 'sanctioned',
-          },
-        },
-        {
-          $lookup: {
-            from: 'SanctionList',
-            localField: 'sanctioned.0.list_id',
-            foreignField: '_id',
-            as: 'sanction',
-            pipeline: [{ $project: { _id: 0, name: 1 } }],
-          },
-        },
-      );
-      //get the DOB joined data in the pipeline
-      //----sanctioned
-      sanctionedPipeline.push({
-        $lookup: {
-          from: 'DateOfBirthList',
-          let: {
-            id: '$_id',
-          },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $eq: ['$sanctionnedId', '$$id'],
-                },
-              },
-            },
-            { $project: { _id: 0, date: 1 } },
-          ],
-          as: 'dateOfBirth',
-        },
-      });
-      //----aka
-      akaPipeline.push({
-        $lookup: {
-          from: 'DateOfBirthList',
-          let: { id: '$sanctionnedId' },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $eq: ['$sanctionnedId', '$$id'],
-                },
-              },
-            },
-            { $project: { _id: 0, date: 1 } },
-          ],
-          as: 'dateOfBirth',
-        },
-      });
-
-      //Get the nationality joined data in the pipeline
-      //----sanctioned
-      sanctionedPipeline.push({
-        $lookup: {
-          from: 'NationalityList',
-          let: {
-            id: '$_id',
-          },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $eq: ['$sanctionnedId', '$$id'],
-                },
-              },
-            },
-            { $project: { _id: 0, country: 1 } },
-          ],
-          as: 'nationality',
-        },
-      });
-      //----aka
-      akaPipeline.push({
-        $lookup: {
-          from: 'NationalityList',
-          let: {
-            id: '$sanctionnedId',
-          },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $eq: ['$sanctionnedId', '$$id'],
-                },
-              },
-            },
-            { $project: { _id: 0, country: 1 } },
-          ],
-          as: 'nationality',
-        },
-      });
-
-      //push project stage to retrieve needed data
-      //----sanctioned
-      sanctionedPipeline.push(
-        {
-          $project: {
-            firstName: 1,
-            middleName: 1,
-            lastName: 1,
-            original_name: 1,
-            otherNames: 1,
-            sanction: {
-              $arrayElemAt: ['$sanction', 0],
-            },
-            dateOfBirth: {
-              $arrayElemAt: ['$dateOfBirth', 0],
-            },
-            nationality: {
-              $arrayElemAt: ['$nationality', 0],
-            },
-            score: { $meta: 'searchScore' },
-          },
-        },
-        { $limit: 20 },
-      );
-
-      akaPipeline.push(
-        {
-          $project: {
-            _id: 0,
-            entity: {
-              $arrayElemAt: ['$sanctioned', 0],
-            },
-            sanction: {
-              $arrayElemAt: ['$sanction', 0],
-            },
-            dateOfBirth: {
-              $arrayElemAt: ['$dateOfBirth', 0],
-            },
-            nationality: {
-              $arrayElemAt: ['$nationality', 0],
-            },
-            score: { $meta: 'searchScore' },
-          },
-        },
-        { $limit: 20 },
-      );
-
       //Request query to mongoDB
       //----sanctioned
       const sanctionedResult: any = await this.prisma.sanctioned.aggregateRaw({
-        pipeline: sanctionedPipeline,
+        pipeline: [
+          {
+            $search: {
+              index: 'sanctionned_index',
+              text: {
+                query: body.fullName,
+                path: ['firstName', 'lastName', 'middleName', 'otherNames'],
+                fuzzy: {
+                  maxEdits: 2,
+                },
+              },
+            },
+          },
+          {
+            $lookup: {
+              from: 'SanctionList',
+              localField: 'list_id',
+              foreignField: '_id',
+              pipeline: [{ $project: { _id: 0, name: 1 } }],
+              as: 'sanction',
+            },
+          },
+          {
+            $lookup: {
+              from: 'DateOfBirthList',
+              let: {
+                id: '$_id',
+              },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $eq: ['$sanctionnedId', '$$id'],
+                    },
+                  },
+                },
+                { $project: { _id: 0, date: 1 } },
+              ],
+              as: 'dateOfBirth',
+            },
+          },
+          {
+            $lookup: {
+              from: 'NationalityList',
+              let: {
+                id: '$_id',
+              },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $eq: ['$sanctionnedId', '$$id'],
+                    },
+                  },
+                },
+                { $project: { _id: 0, country: 1 } },
+              ],
+              as: 'nationality',
+            },
+          },
+          {
+            $project: {
+              firstName: 1,
+              middleName: 1,
+              lastName: 1,
+              original_name: 1,
+              otherNames: 1,
+              sanction: {
+                $arrayElemAt: ['$sanction', 0],
+              },
+              dateOfBirth: {
+                $arrayElemAt: ['$dateOfBirth', 0],
+              },
+              nationality: {
+                $arrayElemAt: ['$nationality', 0],
+              },
+              score: { $meta: 'searchScore' },
+            },
+          },
+          { $limit: 20 },
+        ],
       });
       //----aka
       const akaResult: any = await this.prisma.akaList.aggregateRaw({
-        pipeline: akaPipeline,
+        pipeline: [
+          {
+            $search: {
+              index: 'sanctioned_aka_index',
+              text: {
+                query: body.fullName,
+                path: ['firstName', 'lastName', 'middleName'],
+                fuzzy: {
+                  maxEdits: 2,
+                },
+              },
+            },
+          },
+          {
+            $lookup: {
+              from: 'Sanctioned',
+              let: {
+                id: '$sanctionnedId',
+              },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $eq: ['$_id', '$$id'],
+                    },
+                  },
+                },
+                {
+                  $project: {
+                    list_id: 1,
+                    firstName: 1,
+                    middleName: 1,
+                    lastName: 1,
+                    original_name: 1,
+                    otherNames: 1,
+                  },
+                },
+              ],
+              as: 'sanctioned',
+            },
+          },
+          {
+            $lookup: {
+              from: 'SanctionList',
+              localField: 'sanctioned.0.list_id',
+              foreignField: '_id',
+              as: 'sanction',
+              pipeline: [{ $project: { _id: 0, name: 1 } }],
+            },
+          },
+          {
+            $lookup: {
+              from: 'DateOfBirthList',
+              let: { id: '$sanctionnedId' },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $eq: ['$sanctionnedId', '$$id'],
+                    },
+                  },
+                },
+                { $project: { _id: 0, date: 1 } },
+              ],
+              as: 'dateOfBirth',
+            },
+          },
+          {
+            $lookup: {
+              from: 'NationalityList',
+              let: {
+                id: '$sanctionnedId',
+              },
+              pipeline: [
+                {
+                  $match: {
+                    $expr: {
+                      $eq: ['$sanctionnedId', '$$id'],
+                    },
+                  },
+                },
+                { $project: { _id: 0, country: 1 } },
+              ],
+              as: 'nationality',
+            },
+          },
+          {
+            $project: {
+              _id: 0,
+              entity: {
+                $arrayElemAt: ['$sanctioned', 0],
+              },
+              sanction: {
+                $arrayElemAt: ['$sanction', 0],
+              },
+              dateOfBirth: {
+                $arrayElemAt: ['$dateOfBirth', 0],
+              },
+              nationality: {
+                $arrayElemAt: ['$nationality', 0],
+              },
+              score: { $meta: 'searchScore' },
+            },
+          },
+          { $limit: 20 },
+        ],
       });
 
       //clean data and map before
